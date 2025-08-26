011881b4173efcf86e9e7d38e76fc5af3202aea5
SayedHusain40 <s.husain_10@hotmail.com>
2025-08-26 17:51:54 +0300

    .

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -32,22 +32,3 @@ class Expense {
   }
 }
 
-class ExpenseBucket {
-  ExpenseBucket({required this.expenses, required this.category});
-
-  final Category category;
-  final List<Expense> expenses;
-
-  ExpenseBucket.forCategory(List<Expense> allExpense, this.category)
-    : expenses = allExpense.where((expense) {
-        return expense.category == category;
-      }).toList();
-
-  double get totalExpenseAmount {
-    double sum = 0;
-    for (final expense in expenses) {
-      sum += expense.amount;
-    }
-    return sum;
-  }
-}
