5738904a01f93a449a4f987e6c3888c0ec7256d0
SayedHusain40 <s.husain_10@hotmail.com>
2026-01-11 10:18:05 +0300

    Remove comments on main file that not used

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,6 @@ import 'package:expense_tracker/screens/expense_tracker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]).then((Fn) {
-
-  // });
-
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
